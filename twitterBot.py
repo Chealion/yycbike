52d5e7ec2c4e6ca2eb2499b4af37551379b7bc3d
Micheal Jones <micheal.jones@cybera.ca>
2016-12-31 16:17:17 -0700

    2017 version of Twitter bot ahoy.

--- twitterBot.py
+++ twitterBot.py
@@ -1,13 +1,15 @@
 #! /usr/bin/python
 
-# Downloads data from Eco-Counter, and publishes two tweets based on the data:
-# 1. Total Summary with 3 busiest counters
-# 2. Counts for each section. Midpoints (Median) if there are multiple counters for that part of the network.
+# Downloads data from Eco-Counter, and publishes a series of 4 tweets:
+# 1. Total Summary of on street counters with 3 busiest counters
+# 2. Total Summary of pathway counters with 3 busiest counters
+# 3. Midpoints and counts for On street counters
+# 4. Counts for a selection of other Pathway counters
 #
-
 
 import json
 import urllib2
+import time
 from datetime import date, timedelta
 
 #sixohsix/twitter
@@ -25,111 +27,227 @@ yesterday = date.today() - timedelta(1)
 fancyDate = yesterday.strftime('%a %b %d')
 yesterday = yesterday.strftime('%Y%m%d')
 
-# Wall of URLs
+# Wall of URLs - we want totals. Counters with multiple modes need each direction polled separately
+# Peace Bridge
+PB={'value':0, 'installation':'Peace Bridge', 'url':"null"}
+PB_NB={'installation':"peacebridge", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103013817/"}
+PB_SB={'installation':"peacebridge", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104013817/"}
 
-PB={'value':0, 'name':'Peace Bridge', 'url':"null"}
-PB_NB={'value':0, 'name':'PB_NB', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103013817/"}
-PB_SB={'value':0, 'name':'PB_SB', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104013817/"}
+PB_PED_NB={'installation':"yycwalk.peacebridge", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101013817/"}
+PB_PED_SB={'installation':"yycwalk.peacebridge", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102013817/"}
 
-STEPHEN={'value':0, 'name':'Stephen Ave', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102020207/"}
+SEVENTH={'installation':"7th St", 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100017181/"}
 
-SEVENTH={'value':0, 'name':'7th St', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100017181/"}
+STEPHEN_CAR={'installation':"stephenave", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103020207/"}
+STEPHEN_BIKE={'installation':"Stephen Ave", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102020207/"}
+STEPHEN_PED={'installation':"stephenave", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101020207"}
 
-FIFTH_5={'value':0, 'name':'5 St at 5th', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022540/"}
-FIFTH_10={'value':0, 'name':'5 St at CPR', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100020095/"}
-FIFTH_15={'value':0, 'name':'5 St at 15th', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022541/"}
+FIFTH_5={'installation':"5th and 5th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022540/"}
+FIFTH_10={'installation':"5th at CPR", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100020095/"}
+FIFTH_15={'installation':"5th and 15th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022541/"}
 
-TWELVE_8={'value':0, 'name':'12 Ave at 8th', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022581/"}
-TWELVE_2={'value':0, 'name':'12 Ave at 2nd', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022580/"}
-TWELVE_3={'value':0, 'name':'12 Ave at 3rd', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022582/"}
+TWELFTH_8={'installation':"8th and 12th SW", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022581/"}
+TWELFTH_2={'installation':"2nd and 12th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022580/"}
+TWELFTH_3={'installation':"3rd SE and 12th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100022582/"}
 
-NINTH_4={'value':0, 'name':'9 Ave at 4th', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100030452/"}
-EIGHTH_8={'value':0, 'name':'8 Ave at 8th', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100031441/"}
-EIGHTH_3={'value':0, 'name':'8 Ave at 3rd', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100030768/"}
-TENTH={'value':0, 'name':'10th St', 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100020661/"}
+NINTH_4={'installation':"4th SE and 9th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100030452/"}
+EIGHTH_8={'installation':"8th and 8th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100031441/"}
+EIGHTH_3={'installation':"3rd and 8th", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100030768/"}
 
+RIVERWALK={'value':0, 'installation':'Riverwalk', 'url':"null"}
+RIVERWALK_PED_EB={'installation':"yycwalk.riverwalk", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102128044/"}
+RIVERWALK_PED_WB={'installation':"yycwalk.riverwalk", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101128044/"}
+RIVERWALK_EB={'installation':"riverwalk.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104128044/"}
+RIVERWALK_WB={'installation':"riverwalk.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103128044/"}
 
-total = 0
+NOSECREEK_BOW={'value':0, 'installation':'Nose Creek', 'url':"null"}
+NOSECREEK_BOW_PED_NB={'installation':"yycwalk.nosebow", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102128045/"}
+NOSECREEK_BOW_PED_SB={'installation':"yycwalk.nosebow", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101128045/"}
+NOSECREEK_BOW_NB={'installation':"nosebow.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104128045/"}
+NOSECREEK_BOW_SB={'installation':"nosebow.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103128045/"}
+
+MEMORIAL_PRINCE={'value':0, 'installation':'NBR E of Princes Isl', 'url':"null"}
+MEMORIAL_PRINCE_PED_EB={'installation':"yycwalk.memorialprinces", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102128046/"}
+MEMORIAL_PRINCE_PED_WB={'installation':"yycwalk.memorialprinces", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101128046/"}
+MEMORIAL_PRINCE_EB={'installation':"memorialprinces.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104128046/"}
+MEMORIAL_PRINCE_WB={'installation':"memorialprinces.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103128046/"}
+
+MEMORIAL_19={'value':0, 'installation':'NBR near 19 St NW', 'url':"null"}
+MEMORIAL_19_PED_EB={'installation':"yycwalk.memorialnineteen", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101128047/"}
+MEMORIAL_19_PED_WB={'installation':"yycwalk.memorialnineteen", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102128047/"}
+MEMORIAL_19_EB={'installation':"memorialnineteen.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103128047/"}
+MEMORIAL_19_WB={'installation':"memorialnineteen.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104128047/"}
+
+LINDSAY={'value':0, 'installation':'Lindsay Park', 'url':"null"}
+LINDSAY_PED_NB={'installation':"yycwalk.lindsay", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102128048/"}
+LINDSAY_PED_SB={'installation':"yycwalk.lindsay", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101128048/"}
+LINDSAY_NB={'installation':"lindsay.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104128048/"}
+LINDSAY_SB={'installation':"lindsay.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103128048/"}
+
+TENTH={'installation':"10th St", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100020661/"}
+
+SBOW_10={'value':0, 'installation':'SBR near 11th St', 'url':"null"}
+SBOW_10_PED_EB={'installation':"yycwalk.sbowten", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102030402/"}
+SBOW_10_PED_WB={'installation':"yycwalk.sbowten", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101030402/"}
+SBOW_10_EB={'installation':"sbowten.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/104030402/"}
+SBOW_10_WB={'installation':"sbowten.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103030402/"}
+
+EDM_TR={'installation':"Edmonton Trail", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/100033550/"}
+
+NOSE_HILL={'value':0, 'installation':'Nose Hill', 'url':"null"}
+NOSE_HILL_PED_SB={'installation':"yycwalk.nosehill", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101134303/"}
+NOSE_HILL_PED_NB={'installation':"yycwalk.nosehill", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102134303/"}
+NOSE_HILL_SB={'installation':"nosehill.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134303/"}
+NOSE_HILL_NB={'installation':"nosehill.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134303/"}
+
+PARKDALE={'value':0, 'installation':'NBR @ 29 St', 'url':"null"}
+PARKDALE_PED_EB={'installation':"yycwalk.parkdale", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101134305/"}
+PARKDALE_PED_WB={'installation':"yycwalk.parkdale", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102134305/"}
+PARKDALE_EB={'installation':"parkdale.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134305/"}
+PARKDALE_WB={'installation':"parkdale.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134305/"}
+
+NGLENMORE={'value':0, 'installation':'North Glenmore Park', 'url':"null"}
+NGLENMORE_PED_EB={'installation':"yycwalk.nglenmore", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101134306/"}
+NGLENMORE_PED_WB={'installation':"yycwalk.nglenmore", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102134306/"}
+NGLENMORE_EB={'installation':"nglenmore.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134306/"}
+NGLENMORE_WB={'installation':"nglenmore.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134306/"}
+
+SGLENMORE={'value':0, 'installation':'Glenmore Resv', 'url':"null"}
+SGLENMORE_PED_NB={'installation':"yycwalk.sglenmore", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/101134304/"}
+SGLENMORE_PED_SB={'installation':"yycwalk.sglenmore", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/102134304/"}
+SGLENMORE_NB={'installation':"sglenmore.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134304/"}
+SGLENMORE_SB={'installation':"sglenmore.bike", 'value':0, 'url':"http://www.eco-public.com/api/cw6Xk4jW4X4R/data/periode/103134304/"}
+
+streetTotal = 0
+pathwayTotal = 0
 errorFound = False
 
 # Load Data into Dictionaries
-# Two counters out for winter
-for i in [ PB_NB, PB_SB, STEPHEN, SEVENTH, FIFTH_5, FIFTH_10, FIFTH_15, TWELVE_8, TWELVE_2, TWELVE_3, NINTH_4, EIGHTH_8, EIGHTH_3 ]:
-#for i in [ PB_NB, PB_SB, STEPHEN, SEVENTH, FIFTH_5, FIFTH_10, FIFTH_15, TWELVE_8, TWELVE_2, TWELVE_3, EIGHTH_3 ]:
+for i in [ PB_NB, PB_SB, SEVENTH, STEPHEN_BIKE, FIFTH_5, FIFTH_10, FIFTH_15, TWELFTH_8, TWELFTH_2, TWELFTH_3, NINTH_4, EIGHTH_8, EIGHTH_3, RIVERWALK_EB, RIVERWALK_WB, NOSECREEK_BOW_NB, NOSECREEK_BOW_SB, MEMORIAL_PRINCE_EB, MEMORIAL_PRINCE_WB, MEMORIAL_19_EB, MEMORIAL_19_WB, LINDSAY_NB, LINDSAY_SB, TENTH, SBOW_10_EB, SBOW_10_WB, EDM_TR, NOSE_HILL_SB, NOSE_HILL_NB, PARKDALE_EB, PARKDALE_WB, NGLENMORE_EB, NGLENMORE_WB, SGLENMORE_NB, SGLENMORE_SB ]:
     url=i['url'] + "?begin=" + yesterday + "&end=" + yesterday + "&step=4"
-    response = urllib2.urlopen(url)
-    json_data = response.read()
-    try: 
-        amount = json.loads(json_data)[0]['comptage']
+    amount = 0
+    try:
+        response = urllib2.urlopen(url)
+        json_data = response.read()
+	loaded_data = json.loads(json_data)
+
+	# Handle Case where multiple values are available for the daily total (eg. Lindsay Park counter)
+	if len(loaded_data) > 1:
+		print "More than one entry encountered on %s" % (i['installation'])
+		for entry in loaded_data:
+			if entry['comptage'] is not None:
+				amount += entry['comptage']
+	else:
+		amount = json.loads(json_data)[0]['comptage']
+
+	time.sleep(1)
     except:
 	# If comptage is not there at all - set to 0.
-	amount = 0
-	errorFound = True
-	print "Error loading"
+		amount = 0
+		errorFound = True
+		print "Error loading %s - %s" % (i['installation'], url)
+
     if amount is None or amount == 0:
 	amount = 0
 	errorFound = True
-	print "Error loading - amount is None"
+	print "Error loading - amount is None for %s" % (i['installation'])
     i['value'] = amount
 
-# Create Peace Bridge total
+# Total multi mode counters
 
-PB['value'] = PB_NB['value']+PB_SB['value']
+PB['value'] = PB_NB['value'] + PB_SB['value']
+RIVERWALK['value'] = RIVERWALK_EB['value'] + RIVERWALK_WB['value']
+NOSECREEK_BOW['value'] = NOSECREEK_BOW_NB['value'] + NOSECREEK_BOW_SB['value']
+MEMORIAL_PRINCE['value'] = MEMORIAL_PRINCE_EB['value'] + MEMORIAL_PRINCE_WB['value']
+MEMORIAL_19['value'] = MEMORIAL_19_EB['value'] + MEMORIAL_19_WB['value']
+LINDSAY['value'] = LINDSAY_NB['value'] + LINDSAY_SB['value']
+SBOW_10['value'] = SBOW_10_WB['value'] + SBOW_10_EB['value']
+NOSE_HILL['value'] = NOSE_HILL_NB['value'] + NOSE_HILL_SB['value']
+PARKDALE['value'] = PARKDALE_WB['value'] + PARKDALE_EB['value']
+NGLENMORE['value'] = NGLENMORE_WB['value'] + NGLENMORE_EB['value']
+SGLENMORE['value'] = SGLENMORE_NB['value'] + SGLENMORE_SB['value']
 
-# Create list and sort for top 3.
+# Create list and sort for top 3, street and pathway.
 
-listOfCounters = []
-for i in [ PB, STEPHEN, SEVENTH, FIFTH_5, FIFTH_10, FIFTH_15, TWELVE_8, TWELVE_2, TWELVE_3, NINTH_4, EIGHTH_8, EIGHTH_3 ]:
-#for i in [ PB, STEPHEN, SEVENTH, FIFTH_5, FIFTH_10, FIFTH_15, TWELVE_8, TWELVE_2, TWELVE_3, EIGHTH_3]:
-    listOfCounters.append(i)
-    print "%s: %d" % (i['name'], i['value'])
-    total += i['value']
+listOfStreetCounters = []
+for i in [ STEPHEN_BIKE, SEVENTH, FIFTH_5, FIFTH_10, FIFTH_15, TWELFTH_8, TWELFTH_2, TWELFTH_3, NINTH_4, EIGHTH_8, EIGHTH_3, EDM_TR, TENTH ]:
+    listOfStreetCounters.append(i)
+    print "%s: %d" % (i['installation'], i['value'])
+    streetTotal += i['value']
 
+sortedStreetList = sorted(listOfStreetCounters, key=lambda k: k['value'], reverse=True)
 
-sortedList = sorted(listOfCounters, key=lambda k: k['value'], reverse=True)
-top3String = ""
+listOfPathwayCounters = []
+for i in [ PB, RIVERWALK, NOSECREEK_BOW, MEMORIAL_PRINCE, MEMORIAL_19, LINDSAY, SBOW_10, NOSE_HILL, PARKDALE, NGLENMORE, SGLENMORE ]:
+    listOfPathwayCounters.append(i)
+    print "%s: %d" % (i['installation'], i['value'])
+    pathwayTotal += i['value']
+
+sortedPathwayList = sorted(listOfPathwayCounters, key=lambda k: k['value'], reverse=True)
+
+top3StreetString = ""
+top3PathwayString = ""
 counter = 0
 
 # Grab top 3
-for i in sortedList:
-    top3String += "%s: %d\n" % (i['name'], i['value'])
+for i in sortedStreetList:
+    top3StreetString += "%s: %d\n" % (i['installation'], i['value'])
     counter += 1
     if counter == 3:
         break
 
-# Calculate averages
+counter = 0
 
-#eighthAvg = int(round((EIGHTH_8['value'] + EIGHTH_3['value'] + NINTH_4['value'])/3,0))
-#fifthAvg = int(round((FIFTH_5['value'] + FIFTH_10['value'] + FIFTH_15['value'])/3,0))
-#twelfthAvg = int(round((TWELVE_8['value'] + TWELVE_2['value'] + TWELVE_3['value'])/3,0))
+for i in sortedPathwayList:
+    top3PathwayString += "%s: %d\n" % (i['installation'], i['value'])
+    counter += 1
+    if counter == 3:
+        break
 
-# Use medians as recommended. Done by hand since we have three per.
-
-#eighthAvg = sorted([EIGHTH_8['value'], EIGHTH_3['value'], NINTH_4['value']])[1]
-eighthAvg = EIGHTH_3['value']
-fifthAvg = sorted([FIFTH_5['value'], FIFTH_10['value'], FIFTH_15['value']])[1]
-twelfthAvg = sorted([TWELVE_8['value'], TWELVE_2['value'], TWELVE_3['value']])[1]
-
-# Send out 2 tweets - summary of all, and top hits
-
-## Top Hits
+# Send Tweets
 
 if errorFound:
     flag = '*Some Data Missing'
 else:
     flag = ''
 
-count_status="%d #yycbike trips were counted downtown on %s.\n\nBusiest:\n%s%s" % ( total, fancyDate, top3String, flag)
-print count_status
-
-t.statuses.update(
+## On Street Count with Top 3
+try:
+    count_status="%d #yycbike trips were counted on street on %s.\n\nBusiest:\n%s%s" % ( streetTotal, fancyDate, top3StreetString, flag)
+    print count_status
+    t.statuses.update(
 	status=count_status)
+except:
+    pass
 
-## Summary
-
-count_status="%s #yycbike trip counts:\n\n10 St: %d\n7 St: %d\nPeace Bridge: %d\nStephen Ave: %d\n8 Ave: %d\n5 St Mdn: %d\n12 Ave Mdn: %d" % (fancyDate, TENTH['value'], SEVENTH['value'], PB['value'], STEPHEN['value'], eighthAvg, fifthAvg, twelfthAvg)
-print count_status
-
-t.statuses.update(
+## Pathway Count with Top 3
+try:
+    count_status="%d #yycbike trips were counted on pathways on %s.\n\nBusiest:\n%s%s" % ( pathwayTotal, fancyDate, top3PathwayString, flag)
+    print count_status
+    t.statuses.update(
 	status=count_status)
+except:
+    pass
+
+## Street Raw
+
+try:
+    count_status="%s (Centre Points):\n5 St: %d\n7 St: %d\n8 Ave: %d\n10 St: %d\n12 Ave: %d\nEdm Tr: %d\nStephen Ave: %d" % (fancyDate, FIFTH_10['value'], SEVENTH['value'], EIGHTH_3['value'], TENTH['value'], TWELFTH_2['value'], EDM_TR['value'], STEPHEN_BIKE['value'])
+    print count_status
+
+    t.statuses.update(
+	status=count_status)
+except:
+    pass
+
+## Pathway Raw
+
+try:
+    count_status="%s Other Pathways\nGlenmore Park: %d\nNose Crk: %d\nLindsay: %d\nPrince's Isl: %d\nNose Hill: %d\n19th NW: %d\nParkdale: %d\n" % (fancyDate, SGLENMORE['value'], NOSECREEK_BOW['value'], LINDSAY['value'], MEMORIAL_PRINCE['value'], NOSE_HILL['value'], MEMORIAL_19['value'], PARKDALE['value'])
+    print count_status
+
+    t.statuses.update(
+	status=count_status)
+except:
+    pass
 
